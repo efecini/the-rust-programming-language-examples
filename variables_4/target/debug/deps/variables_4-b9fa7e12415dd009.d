f63717e9e938c7f2ca64af51792afa6d6b75a235
efecini <efecini@gmail.com>
2025-02-09 02:58:34 +0300

    add chapter 4th project

---
+++ variables_4/target/debug/deps/variables_4-b9fa7e12415dd009.d
@@ -0,0 +1,5 @@
+/Users/efecini/Projects/rust/the-rust-programming-language-examples/variables_4/target/debug/deps/variables_4-b9fa7e12415dd009: src/main.rs
+
+/Users/efecini/Projects/rust/the-rust-programming-language-examples/variables_4/target/debug/deps/variables_4-b9fa7e12415dd009.d: src/main.rs
+
+src/main.rs:
---
+++ variables_4/target/debug/variables_4.d
@@ -0,0 +1,1 @@
+/Users/efecini/Projects/rust/the-rust-programming-language-examples/variables_4/target/debug/variables_4: /Users/efecini/Projects/rust/the-rust-programming-language-examples/variables_4/src/main.rs
