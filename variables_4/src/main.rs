5f7472b09c5107b6a63938b6b8c75376a6f84db4
efecini <efecini@gmail.com>
2025-02-09 23:52:14 +0300

    constants1

--- variables_4/src/main.rs
+++ variables_4/src/main.rs
@@ -1,6 +1,10 @@
+// Always immutable, can't use mut, can be declred in any scope.
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is {x}");
     x = 6;
     println!("The value of x is {x}");
+    println!("{THREE_HOURS_IN_SECONDS}");
 }
